cb67633d969a126d0bdb220dbf7e0c5a86086534
Lloyd Garrison <lloyd.h.garrison@gmail.com>
2017-01-11 14:25:25 -0600

    deleting DaoFactory and PostInterface; Adding PostRepository and TagRepository

---
+++ src/main/java/com/codeup/repository/TagRepository.java
@@ -0,0 +1,9 @@
+package com.codeup.repository;
+
+import com.codeup.models.Tag;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TagRepository extends JpaRepository<Tag, Long> {
+
+    Tag findByNameIgnoreCase(String name);
+}
