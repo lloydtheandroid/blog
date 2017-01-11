6b7d5cd0621f2c05e9275d6202585af37916c85c
Lloyd Garrison <lloyd.h.garrison@gmail.com>
2017-01-11 09:06:50 -0600

    adding dependencies for markdown and jsoup; create Markdown converter;

--- src/main/java/com/codeup/dao/RolesInterface.java
+++ src/main/java/com/codeup/dao/RolesInterface.java
@@ -14,5 +14,5 @@ public interface RolesInterface extends JpaRepository<UserRole, Long> {
     UserRole findByName(String name);
 
     @Query("select ur.role from UserRole ur, User u where u.username=?1 and ur.userId = u.id")
-    public List<String> ofUserWith(String username);
+    List<String> ofUserWith(String username);
 }
