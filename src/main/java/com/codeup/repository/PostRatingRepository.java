e0c60a0a2bc58f3bf14a4f3977007cb6f0765270
Lloyd Garrison <lloyd.h.garrison@gmail.com>
2017-01-11 14:17:03 -0600

    created Post Rating repository

---
+++ src/main/java/com/codeup/repository/PostRatingRepository.java
@@ -0,0 +1,13 @@
+package com.codeup.repository;
+
+
+import com.codeup.models.PostRating;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+public interface PostRatingRepository extends JpaRepository<PostRating, Long> {
+
+    @Query("SELECT r FROM PostRating r WHERE r.post.id = :postId AND r.user.id = :userId")
+    PostRating findUserRating(@Param("postId") Long postId, @Param("userId") Long userId);
+}
